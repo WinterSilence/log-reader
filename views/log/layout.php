d753817af0541dfa8324c9a588c817a9b0290ece
WinterSilence <info@handy-soft.ru>
2013-08-24 15:55:33 +0400

    small fixes

--- views/log/layout.php
+++ views/log/layout.php
@@ -3,32 +3,32 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Kohana Log Viewer</title>
-		<base>
+		<base href="/">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<style media="all" type="text/css">
-		<?php include_once MODPATH .'log/media/bootstrap.min.css' ?>
-		<?php include_once MODPATH .'log/media/style.css' ?>
+		<?php include_once MODPATH.'log/media/bootstrap.min.css' ?>
+		<?php include_once MODPATH.'log/media/style.css' ?>
 		</style>
-		
-		<script type="text/javascript"><?php include_once MODPATH .'log/media/jquery.min.js' ?></script>
-		
+		<script type="text/javascript">
+		<?php include_once MODPATH.'log/media/jquery.min.js' ?>
+		</script>
 	</head>
 	<body>
-		
-	<div class="container">
-		<div class="content">
-			<div class="row">
-				<div class="span2">
-					<div class="well sidebar-nav">
-					<?php echo View::factory('log/menu')->set('logs', $log->logs)->set('param', $param) ?>
+		<div class="container">
+			<div class="content">
+				<div class="row">
+					<div class="span2">
+						<div class="well sidebar-nav">
+						<?php echo View::factory('log/menu')
+										->set('logs', $log->logs)
+										->set('param', $param) ?>
+						</div>
 					</div>
-				</div>
-				<div class="span10">
-					<?php echo $content ?>
-				</div>
-			</div> <!-- /container -->
-		</div> 
-	</div> 
-		
+					<div class="span10">
+						<?php echo $content ?>
+					</div>
+				</div><!-- /container -->
+			</div>
+		</div>
 	</body>
 </html>
