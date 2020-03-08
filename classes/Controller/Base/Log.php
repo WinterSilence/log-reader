740fc09cb4f9fb6af6b9b808871e81dfd5e7bee3
Anton <info@ensostudio.ru>
2020-03-08 14:11:19 +0300

    Update Log.php
    update to 3.4

--- classes/Controller/Base/Log.php
+++ classes/Controller/Base/Log.php
@@ -1,45 +1,46 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
+<?php
+/**
+ * Controller to display system logs.
+ */
 abstract class Controller_Base_Log extends Controller_Template
 {
-	
-	public $template = NULL;
-	
+	/**
+	 * @var Model_Log_Reader
+	 */
+	protected $log_reader;
 
-	
 	public function before()
 	{
 		parent::before();
-		$this->template->param = Arr::extract($this->request->param(), array('year', 'month', 'day'));
+		$this->template->param = Arr::extract($this->request->param(), ['year', 'month', 'day']);
+		$this->log_reader = new Model_Log_Reader();
 	}
 	
-	public function action_show()
+	public function action_index()
 	{
 		$this->template->param['level'] = $this->request->post('level');
-		
-		$this->template->log = Model::factory('Log_Reader')->set_config($this->template->param);
+		$this->template->log = $this->log_reader->set_config($this->template->param);
 	}
 	
 	public function action_delete()
 	{
-		$this->auto_render = FALSE;
-		
-		Model::factory('Log_Reader')->set_config($this->template->param)->delete();
-		
-		HTTP::redirect(Route::url('log'));
+		$this->auto_render = false;
+		$this->log_reader->set_config($this->template->param)->delete();
+		static::redirect($this->request->route->uri(['controller' => 'Base_Log']));
 	}
 	
 	public function after()
 	{
 		if ($this->auto_render)
 		{
-			$content = 'log'.DIRECTORY_SEPARATOR.$this->request->action();
+			$path_prefix = 'log' . DIRECTORY_SEPARATOR;
+			$content = $path_prefix . $this->request->action();
 			$this->template->content = $this->template->render($content);
 			
-			$layout = ($this->request->is_initial() ? 'layout' : 'layout-min');
-			$this->template->set_filename('log'.DIRECTORY_SEPARATOR.$layout);
+			$layout = $this->request->is_initial() ? 'layout' : 'layout-min';
+			$this->template->set_filename($path_prefix . $layout);
 		}
+		
 		parent::after();
-	}
-	
-} // End Controller Log
+	}	
+}
